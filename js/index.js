305136568136592ea4e2096949e284dc662e026b
ElaineTavares <etsets.tavares@gmail.com>
2024-03-06 11:14:13 -0300

    atualizar

--- js/index.js
+++ js/index.js
@@ -39,16 +39,39 @@ window.addEventListener('scroll', () =>{
 
 
 //ALTERAR BACKGROUND
-setInterval(function mudarBackground(){   
-    let background = document.querySelector('.principal')
-   background.style.backgroundImage = "url('../img/condominio_eco_vila_talisia_pronto_para_morar1.jpeg')"    
-},9000) 
+//setInterval(function mudarBackground(){   
+//    let background = document.querySelector('.principal')
+//   background.style.backgroundImage = "url('../img/condominio_eco_vila_talisia_pronto_para_morar1.jpeg')"    
+//},9000) 
 
-setInterval(function mudarBackground2(){
-    let background2 = document.querySelector('.principal')
-    background2.style.backgroundImage = "url('../img/praia_residencia.jpg')"  
-},10000)
+//setInterval(function mudarBackground2(){
+//    let background2 = document.querySelector('.principal')
+//    background2.style.backgroundImage = "url('../img/praia_residencia.jpg')"  
+//},10000)
 //FIM ALTERAR BACKGROUND
+
+
+setInterval(function alterarBackground() {
+    var elemento = document.querySelector('.principal')
+    var imagens = [
+        'url("../img/condominio_eco_vila_talisia_pronto_para_morar1.jpeg")',
+        'url("../img/praia_residencia.jpg")' ,
+        //'url("../img/condominio_portais_dos_rubis.png")'
+    ];
+    var indiceAtual = imagens.indexOf(elemento.style.backgroundImage);
+
+    // Verifica se o índice atual é o último da lista, se for, volta para o início
+    if (indiceAtual === imagens.length - 1) {
+        indiceAtual = 0;
+    } else {
+        indiceAtual++;
+    }
+
+    // Define a próxima imagem de fundo
+    elemento.style.backgroundImage = imagens[indiceAtual];
+},5000);
+
+
 
 
 
